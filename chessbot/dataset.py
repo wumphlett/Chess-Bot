b4131c95bed4c67f6a7b4ed338641578b3f465be
Will Humphlett <will@humphlett.net>
2022-12-26 17:38:33 -0500

    Correctly defined pos2vec

--- chessbot/dataset.py
+++ chessbot/dataset.py
@@ -1,10 +1,39 @@
 import math
+import random
 
 import numpy as np
 import pandas as pd
 from tensorflow.keras.utils import Sequence
 
 from chessbot.environment import LOSS_DATASET, LOSS_VAL_DATASET, WIN_DATASET, WIN_VAL_DATASET
+
+
+class DeepChessDataset(Sequence):
+    def __init__(self, batch_size):
+        self.x = np.empty(shape=(1_000_000, 2, 773), dtype=bool)
+        self.y = np.empty(shape=(1_000_000, 2), dtype=int)
+        self.win = pd.read_pickle(WIN_DATASET)
+        self.loss = pd.read_pickle(LOSS_DATASET)
+        self.batch_size = batch_size
+        self.on_epoch_end()
+
+    def __len__(self):
+        return math.ceil(len(self.x) / self.batch_size)
+
+    def __getitem__(self, idx):
+        x_batch = self.x[idx * self.batch_size:(idx + 1) * self.batch_size]
+        y_batch = self.y[idx * self.batch_size:(idx + 1) * self.batch_size]
+        return x_batch, y_batch
+
+    def on_epoch_end(self):
+        win_sample = self.win.sample(1_000_000)["bitboard"]
+        loss_sample = self.loss.sample(1_000_000)["bitboard"]
+        for i, (x_win, x_loss) in enumerate(zip(win_sample.values, loss_sample.values)):
+            label = random.choice(((1, 0), (0, 1)))
+            position_pair = (x_win, x_loss) if label == (1, 0) else (x_loss, x_win)
+
+            self.x[i] = position_pair
+            self.y[i] = label
 
 
 class Pos2VecDataset(Sequence):
@@ -21,6 +50,10 @@ class Pos2VecDataset(Sequence):
 
     def on_epoch_end(self):
         np.random.shuffle(self.x)
+
+
+def deepchess_dataset():
+    return DeepChessDataset(50)
 
 
 def pos2vec_dataset():
--- chessbot/model.py
+++ chessbot/model.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 import tensorflow as tf
 from tensorflow.keras.callbacks import LearningRateScheduler
-from tensorflow.keras.layers import Dense, LeakyReLU
+from tensorflow.keras.layers import Dense, Input, LeakyReLU
 from tensorflow.keras.losses import BinaryCrossentropy
 from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.optimizers import Adam, SGD
@@ -16,40 +16,40 @@ POS2VEC_LAYERS = [600, 400, 200, 100]
 class Pos2Vec(Model):
     def __init__(self):
         super().__init__()
-        self.encode = Sequential(
+        self.encoder = Sequential(
             [
-                Dense(773, activation=LeakyReLU()),
-                Dense(600, activation=LeakyReLU()),
-                Dense(400, activation=LeakyReLU()),
-                Dense(200, activation=LeakyReLU()),
-                Dense(100, activation=LeakyReLU()),
-            ]
+                Input(shape=(773,)),
+                Dense(600, activation="relu", name="encoder_1"),
+                Dense(400, activation="relu", name="encoder_2"),
+                Dense(200, activation="relu", name="encoder_3"),
+                Dense(100, activation="relu", name="encoder_4"),
+            ], name="p2v_encoder"
         )
 
         if POS2VEC_WEIGHTS.exists():
-            self.build((None, 773))
-            self.encode.load_weights(POS2VEC_WEIGHTS)
+            print(f"Pos2Vec weights found at {POS2VEC_WEIGHTS}, loading")
+            self.encoder.load_weights(POS2VEC_WEIGHTS)
 
     def call(self, x, **kwargs):
-        return self.encode(x)
+        return self.encoder(x)
 
 
 class AutoEncoder(Model):
     def __init__(self):
         super().__init__()
-        self.encode = Sequential(
+        self.encoder = Sequential(
             [
-                Dense(773, activation=LeakyReLU(), name="encode_1"),
-            ]
+                Input(shape=(773,)),
+            ], name="ae_encoder"
         )
-        self.decode = Sequential(
+        self.decoder = Sequential(
             [
-                Dense(773, activation="sigmoid", name="decode_4"),
-            ]
+                Dense(773, activation="sigmoid", name="decoder_4"),
+            ], name="ae_decoder"
         )
 
     def call(self, x, **kwargs):
-        return self.decode(self.encode(x))
+        return self.decoder(self.encoder(x))
 
     @staticmethod
     def lr_schedule(epoch, lr):
@@ -66,19 +66,21 @@ def train_pos2vec(x_train, x_val):
     for i, size in enumerate(POS2VEC_LAYERS):
         print(f"Training layer {i+1}/{len(POS2VEC_LAYERS)}")
 
-        ae.encode.add(Dense(size, activation=LeakyReLU(), name=f"encode_{i+2}"))
+        ae.encoder.add(Dense(size, activation="relu", name=f"encoder_{i+1}"))
         if i != 0:
-            layers = ae.decode.layers
-            ae.decode = Sequential([Dense(POS2VEC_LAYERS[i-1], activation=LeakyReLU(), name=f"decode_{4-i}")] + layers)
+            layers = ae.decoder.layers
+            ae.decoder = Sequential([Dense(POS2VEC_LAYERS[i-1], activation="relu", name=f"decoder_{4-i}")] + layers)
 
         ae.compile(optimizer=SGD(learning_rate=0.005), loss=BinaryCrossentropy(), jit_compile=True)
         ae.fit(x_train, epochs=200, callbacks=[LearningRateScheduler(AutoEncoder.lr_schedule)], workers=8, validation_data=x_val)
 
-        for j, layer in enumerate(ae.encode.layers):
-            layer.trainable = False
-        for j, layer in enumerate(ae.decode.layers):
-            layer.trainable = False
+        ae.encoder.summary()
+        ae.decoder.summary()
 
+        for j, layer in enumerate(ae.encoder.layers):
+            layer.trainable = False
+        for j, layer in enumerate(ae.decoder.layers):
+            layer.trainable = False
     ae.encode.save_weights(POS2VEC_WEIGHTS)
 
     return Pos2Vec()
